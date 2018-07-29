4bf80aa0c2b681e9a3c737e26743a3bb9abcfd48
sihwan <k.sihwan@gmail.com>
2018-07-29 15:39:30 -0400

    a

---
+++ Hello_world_LED/main.c
@@ -0,0 +1,20 @@
+// Sihwan Kim
+// Jul. 29, 2018
+
+#include <msp430.h>
+
+int main(void)
+{
+  int i;
+
+  WDTCTL = WDTPW+WDTHOLD;                   // Stop WDT
+  P1DIR |= BIT0;                            // P1.0 set as output
+
+  while(1)                                  // continuous loop
+  {
+    P1OUT ^= BIT0;                          // XOR P1.0
+    //for(i=0;i<0xFFFF;i++);                   // Delay
+    for(i=0;i<0xFFFF;i++);                   // Delay
+  }
+}
+
